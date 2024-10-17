8ae88eea7d7ce0b657f3c0acbc5a9a504894aa43
Abdul Haseeb <122510812+AbdulHaseeb2004@users.noreply.github.com>
2024-10-17 09:05:19 +0530

    Sum of Fibonacci series

---
+++ Easy/Sum of Fibonacci series.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenFibonacciSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N value: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, sum = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum += a;
+            }
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+        System.out.println("Even Sum: " + sum);
+    }
+}

4a127fe0f07b6a2c09abff97fecdc5f7cdf32f00
Abdul Haseeb <122510812+AbdulHaseeb2004@users.noreply.github.com>
2024-10-19 09:17:01 +0530

    Reduce to zero

---
+++ Hard/Reduce to zero.java
@@ -0,0 +1,19 @@
+public class StepsToZero {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        StepsToZero stz = new StepsToZero();
+        System.out.println(stz.numberOfSteps(14));  // Output: 6
+    }
+}
